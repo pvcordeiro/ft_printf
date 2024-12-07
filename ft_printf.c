d37ddbc5d07fd616a6a6896d6aff346d3ef4dddf
Paulo Victor <pv.35@hotmail.com>
2024-12-07 10:28:53 +0000

    change helpers to static

--- ft_printf.c
+++ ft_printf.c
@@ -6,34 +6,58 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 20:14:06 by paude-so          #+#    #+#             */
-/*   Updated: 2024/11/08 21:18:58 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/12/07 10:24:27 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *input, ...)
+static int	pv_putstr(char *s)
 {
-	int		count;
-	va_list	args;
-
-	va_start(args, input);
-	count = 0;
-	while (*input)
-	{
-		if (*input == '%' && *(input + 1) != '%' && *(input + 1))
-			count += pv_parse_args(*(++input), args);
-		else if (*input == '%' && *(input + 1) == '%')
-			count += write(1, ++input, 1);
-		else
-			count += write(1, input, 1);
-		input++;
-	}
-	va_end(args);
-	return (count);
+	if (!s)
+		return (pv_putstr("(null)"));
+	if (!*s)
+		return (0);
+	return (write(1, s, 1) + pv_putstr(s + 1));
 }
 
-int	pv_parse_args(char letter, va_list args)
+static int	pv_putaddress(void *p, int prefix)
+{
+	int				count;
+	unsigned long	n;
+
+	count = 0;
+	n = (unsigned long)p;
+	if (!n)
+		return (pv_putstr("(nil)"));
+	if (prefix)
+		count += pv_putstr("0x");
+	if (n >= 16)
+		count += pv_putaddress((void *)(n / 16), 0);
+	return (count += write(1, &"0123456789abcdef"[n % 16], 1));
+}
+
+static int	pv_puthex_n_deci(long n, int base, int upper)
+{
+	int		count;
+	char	*digits;
+
+	count = 0;
+	if (n < 0)
+	{
+		n = -n;
+		count += write(1, "-", 1);
+	}
+	if (upper)
+		digits = "0123456789ABCDEF";
+	else
+		digits = "0123456789abcdef";
+	if (n >= base)
+		count += pv_puthex_n_deci(n / base, base, upper);
+	return (count += write(1, &digits[n % base], 1));
+}
+
+static int	pv_parse_args(char letter, va_list args)
 {
 	int		count;
 	char	c;
@@ -59,47 +83,23 @@ int	pv_parse_args(char letter, va_list args)
 	return (count);
 }
 
-int	pv_putstr(char *s)
-{
-	if (!s)
-		return (pv_putstr("(null)"));
-	if (!*s)
-		return (0);
-	return (write(1, s, 1) + pv_putstr(s + 1));
-}
-
-int	pv_putaddress(void *p, int prefix)
-{
-	int				count;
-	unsigned long	n;
-
-	count = 0;
-	n = (unsigned long)p;
-	if (!n)
-		return (pv_putstr("(nil)"));
-	if (prefix)
-		count += pv_putstr("0x");
-	if (n >= 16)
-		count += pv_putaddress((void *)(n / 16), 0);
-	return (count += write(1, &"0123456789abcdef"[n % 16], 1));
-}
-
-int	pv_puthex_n_deci(long n, int base, int upper)
+int	ft_printf(const char *input, ...)
 {
 	int		count;
-	char	*digits;
+	va_list	args;
 
+	va_start(args, input);
 	count = 0;
-	if (n < 0)
+	while (*input)
 	{
-		n = -n;
-		count += write(1, "-", 1);
+		if (*input == '%' && *(input + 1) != '%' && *(input + 1))
+			count += pv_parse_args(*(++input), args);
+		else if (*input == '%' && *(input + 1) == '%')
+			count += write(1, ++input, 1);
+		else
+			count += write(1, input, 1);
+		input++;
 	}
-	if (upper)
-		digits = "0123456789ABCDEF";
-	else
-		digits = "0123456789abcdef";
-	if (n >= base)
-		count += pv_puthex_n_deci(n / base, base, upper);
-	return (count += write(1, &digits[n % base], 1));
+	va_end(args);
+	return (count);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 19:39:58 by paude-so          #+#    #+#             */
-/*   Updated: 2024/11/08 21:19:12 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/12/07 10:23:54 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,8 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include <stdlib.h>
 # include <unistd.h>
 
 int	ft_printf(const char *input, ...);
-int	pv_parse_args(char letter, va_list args);
-int	pv_putstr(char *s);
-int	pv_putaddress(void *pointer, int prefix);
-int	pv_puthex_n_deci(long n, int base, int upper);
 
 #endif
